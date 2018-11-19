d3f9af37eed82e8d23469f7f4303aa983f341341
Jessica Chernyak <jessicachernyak@gmail.com>
2018-11-19 14:43:12 -0600

    Restored code in School

---
+++ School/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School
+{
+    public class Student
+    {
+        public string name;
+        public int studentId;
+        private readonly int numberOfCredits;
+        private double gpa;
+    }
+
+    public class Course
+    {
+        public readonly int classId;
+        public readonly string className;
+        public Student[] enrolledStudents;
+
+    }
+}
